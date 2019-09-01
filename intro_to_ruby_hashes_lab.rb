d87ddeefe66aeae159f5b5272fef1e5e38035475
Vanna Winland <vgwinland@gmail.com>
2019-09-01 18:10:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  my_hash_creator = {:name => 'Grace Hopper'}, id
+  my_hash_creator ={ {:name => 'Grace Hopper'}, :id => 2}}
 end
 
 def read_from_hash(hash, key)
